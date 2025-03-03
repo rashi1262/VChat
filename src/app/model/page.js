db89d31e993b68a88af0d79b712cb770bbd50e7d
Lakshit Singhal <viddeveloper772@gmail.com>
2025-03-03 12:02:07 +0530

    code done

--- src/app/model/page.js
+++ src/app/model/page.js
@@ -22,25 +22,6 @@ const page = ({ params }) => {
 
   const router = useRouter();
 
-  // 🔹 **Get Logged-in User Data**
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      try {
-        const storedUser = localStorage.getItem("user");
-        if (!storedUser) {
-          router.push("/login");
-          return;
-        } else {
-          const user = JSON.parse(storedUser);
-          setEmail(user?.email || "No Email");
-          setName(user?.name || "No Name");
-          setUserId(user?.id);
-        }
-      } catch (error) {
-        console.error("Error reading user data:", error);
-      }
-    }
-  }, []);
 
   useEffect(() => {
     if (!userId) return;
