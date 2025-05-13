5a28aea688b928e5977a20694e90045375e1ae1c
unknown <viddeveloper111@gmail.com>
2025-05-13 11:18:40 +0530

    dev

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import logo from "../../../public/assests/logo.jpg";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
